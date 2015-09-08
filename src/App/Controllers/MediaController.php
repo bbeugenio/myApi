92cf5fd32aad933f6575da5af019ea285ab7e1f1
bbeugenio <bertonibrunoeugenio@gmail.com>
2015-09-08 00:09:39 -0300

    changes in method getPhotoById and mediaService class was deleted
    The getPhotoById  method now returns the mediaId with its Location and
    Nearest places. With these modifications, the mediaService class was
    useless, so I deleted it.

--- src/App/Controllers/MediaController.php
+++ src/App/Controllers/MediaController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controllers;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Services\mediaService;
 
 class MediaController
 {
@@ -14,59 +13,65 @@ class MediaController
 
     public function getPhotoById($id)
     {
-        $url = "https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=5fa500be04134056ab745cc48cf0382f&max_tag_id=" . $id;
-        $urlPlaces = "";
-        $str = file_get_contents($url);
-        $json = json_decode($str, true);
+        $url = file_get_contents("https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=5fa500be04134056ab745cc48cf0382f&max_tag_id=" . $id);
+        $jsonOrg = json_decode($url, true);
 
-        $idPhoto = $id;
-        $latitude = $json['data'][0]['location']['latitude'];  
-        $longitude = $json['data'][0]['location']['longitude'];  
-        
-        $urlPlaces = "https://api.instagram.com/v1/locations/search?lat=".$latitude."&lng=".$longitude. "&client_id=5fa500be04134056ab745cc48cf0382f";
-        $aux = file_get_contents($urlPlaces);
-        $jsonPlaces = json_decode($aux, true);
+        $latitude = $jsonOrg['data'][0]['location']['latitude'];  
+        $longitude = $jsonOrg['data'][0]['location']['longitude'];  
+
+        $urlPlaces = file_get_contents("https://api.instagram.com/v1/locations/search?lat=".$latitude."&lng=".$longitude. "&client_id=5fa500be04134056ab745cc48cf0382f");
+        $jsonPlaces = json_decode($urlPlaces, true);
         $length = count($jsonPlaces['data']);
 
-        $array = array();
+        $idPlace = $jsonPlaces['data'][0]['id'];
+        $latitude = $jsonPlaces['data'][0]['latitude'];  
+        $longitude = $jsonPlaces['data'][0]['longitude'];
+        $namePlace = $jsonPlaces['data'][0]['name'];
+        $address = $this->getAddressByLatitudeLongitude($latitude,$longitude);
+
+        $arrayMedia = array();
+        $arrayMedia['Id'] = $id;
+        $arrayMedia['Location'][0]['Id'] = $idPlace;
+        $arrayMedia['Location'][0]['Geopoint'][0]['Latitude'] = $latitude;
+        $arrayMedia['Location'][0]['Geopoint'][0]['Longitude'] = $longitude;
+        $arrayMedia['Location'][0]['Place'][0]['Name'] = $namePlace;
+        $arrayMedia['Location'][0]['Place'][0]['Address'] = $address;
+
+        $arrayNearest = array();
+        $isFirst = true;
         for ($i = 0; $i < $length; $i++) {
-            $idPhoto = "";
-            $latitude = "";
-            $longitude = "";  
-            $place = ""; 
-
-            $idPhoto = $id;
-            if(!is_null($jsonPlaces['data'][$i]['latitude'] ))
+            if($isFirst)
             {
+                $isFirst = false;
+            }
+            else
+            {
+                $idPlace = $jsonPlaces['data'][$i]['id'];
                 $latitude = $jsonPlaces['data'][$i]['latitude'];  
-            }
-            if(!is_null($jsonPlaces['data'][$i]['longitude'] ))
-            {
-                $longitude = $jsonPlaces['data'][$i]['longitude'];  
-            }
-            if(!is_null($jsonPlaces['data'][$i]['name']))
-            {
-                $address = $jsonPlaces['data'][$i]['name'];  
-            }
-            $objeto = new mediaService($idPhoto,$latitude,$longitude,$address);
+                $longitude = $jsonPlaces['data'][$i]['longitude'];
+                $namePlace = $jsonPlaces['data'][$i]['name'];
+                $address = $this->getAddressByLatitudeLongitude($latitude,$longitude);
 
-            array_push($array, $objeto);
+                $arrayMyNearest = array(); 
+                $arrayMyNearest['Location'][0]['Id'] = $idPlace;
+                $arrayMyNearest['Location'][0]['Geopoint'][0]['Latitude'] = $latitude;
+                $arrayMyNearest['Location'][0]['Geopoint'][0]['Longitude'] = $longitude;
+                $arrayMyNearest['Location'][0]['Place'][0]['Name'] = $namePlace;
+                $arrayMyNearest['Location'][0]['Place'][0]['Address'] = $address;
+                
+                array_push($arrayNearest, $arrayMyNearest);
+            }
         }
+        $arrayMedia['Nearest Places'] = $arrayNearest;
 
-        return new JsonResponse($array);
+        return new JsonResponse($arrayMedia);
     }
 
-    public function getJson()
+    public function getAddressByLatitudeLongitude($latitude, $longitude)
     {
-        $str = file_get_contents("https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=5fa500be04134056ab745cc48cf0382f");
-        $json = json_decode($str, true);
-        //echo '<pre>' . print_r($json, true) . '</pre>';  
-        $tag = $json['data'][0]['tags'];  
-        $tags = "";
-        foreach ($json['data'][0]['tags'] as $field => $value) {
-            $tags .= $value . "\n";    
-        }
-        //echo '<pre>' . print_r($tags) . '</pre>';  
-        return new JsonResponse($tag);
+        $urlAddress = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?latlng=".$latitude.",".$longitude."&sensor=true");
+        $json = json_decode($urlAddress, true);
+        $address = $json['results'][0]['formatted_address'];  
+        return $address;
     }
 }
--- src/App/RoutesLoader.php
+++ src/App/RoutesLoader.php
@@ -18,10 +18,12 @@ class RoutesLoader
     private function instantiateControllers()
     {
     
-        $this->app['media.controller'] = $this->app->share(function () 
-        {
-            return new Controllers\MediaController();
-        });
+        $this->app['media.controller'] = $this->app->share(
+            function () 
+            {
+                return new Controllers\MediaController();
+            }
+        );
     }
 
     public function bindRoutesToControllers()
@@ -31,7 +33,7 @@ class RoutesLoader
         $api->get('/json', "media.controller:getJson");
         $api->get('/photo/{id}', "media.controller:getPhotoById");
 
-        $this->app->mount($this->app["api.endpoint"].'/'/*.$this->app["api.version"]*/, $api);
+        $this->app->mount($this->app["api.endpoint"].'/', $api);
     }
 }
 
