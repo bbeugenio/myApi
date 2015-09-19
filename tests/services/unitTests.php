d0a1dcf4ebaaebbb292831a804d3ced7b813a5b9
bbeugenio <bertonibrunoeugenio@gmail.com>
2015-09-18 21:43:41 -0300

    testMediaControllerClass was added
    A new test was added to control mediaController Class.

--- tests/services/unitTests.php
+++ tests/services/unitTests.php
@@ -12,7 +12,10 @@ use App\Controllers\MediaController;
 
 class unitTests extends \PHPUnit_Framework_TestCase
 {
-
+    /*** 
+        Method: testGeopointClass
+        Description: This method test that geopoint's constructor, the set and get method are working fine.
+    ***/
     public function testGeopointClass()
     {
         $olapicLatitude = "-31.4256195";
@@ -26,6 +29,10 @@ class unitTests extends \PHPUnit_Framework_TestCase
         $this->assertEquals($olapicLongitude, $geopoint->__get("longitude"));
     }
 
+    /*** 
+        Method: testMapClass
+        Description: This method test that map's constructor, the set and get method are working fine.
+    ***/
     public function testMapClass()
     {
         $olapicImage = "http://maps.googleapis.com/maps/api/staticmap?center=-31.4256195%20-64.1876011&zoom=15&scale=false&size=640x480&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff3900%7Clabel:A%7C-31.4256195%20-64.1876011";
@@ -36,6 +43,10 @@ class unitTests extends \PHPUnit_Framework_TestCase
         $this->assertEquals($olapicImage, $map->__get("image"));
     }
 
+    /*** 
+        Method: testPlaceClass
+        Description: This method test that place's constructor, the set and get method are working fine.
+    ***/
     public function testPlaceClass()
     {
         $olapicName = "Olapic Argentina S.A";
@@ -49,6 +60,10 @@ class unitTests extends \PHPUnit_Framework_TestCase
         $this->assertEquals($olapicAddress, $place->__get("address"));
     }
 
+    /*** 
+        Method: testLocationClass
+        Description: This method test that location's constructor, the set and get method are working fine.
+    ***/
     public function testLocationClass()
     {
         $olapicId = "X5000GXB";
@@ -67,5 +82,29 @@ class unitTests extends \PHPUnit_Framework_TestCase
         $this->assertEquals($olapicName, $olapicLocation->__get("name"));
         $this->assertEquals($olapicAddress, $olapicLocation->__get("address"));
         $this->assertEquals($olapicMap, $olapicLocation->__get("image"));
+    }
+
+    /*** 
+        Method: testMediaControllerClass
+        Description: This method test that json's value is not null, that json is an object, that json dont have any errors and that json is a valid Json. 
+    ***/
+    public function testMediaControllerClass()
+    {
+        $mediaController = new mediaController();
+        $json = $mediaController->getInformationPhotoById("1062276093275560411");
+        $this->assertNotNull($json);
+
+        $succesful = true;
+        if(!is_object($json))
+            $succesful = false;
+        $this->assertTrue($succesful);
+
+        if(json_last_error() == JSON_ERROR_NONE)
+            $succesful = true;
+        else
+            $succesful = false;
+        $this->assertTrue($succesful);
+
+        $this->assertJson(json_encode($json));
     }
 }
